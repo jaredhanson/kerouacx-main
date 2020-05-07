f26a822b2a5ec6981ac2f8940d9a6f6901106568
Jared Hanson <jaredhanson@gmail.com>
2020-05-07 16:28:36 -0700

    Documentation and better error handling.

--- app/main.js
+++ app/main.js
@@ -1,11 +1,27 @@
+/**
+ * Main script.
+ *
+ * This component provides a main script that generates a site using the Kerouac
+ * framework.
+ *
+ * This package provides a default site component which eliminates the
+ * boilerplate typically used in most sites.  It is expected that most sites
+ * will take advantage of this capability.  However, the main script will
+ * preferrentially load a site-specific component, accomodating sites that need
+ * to override the standard boilerplate.
+ *
+ * Once the site is created, static files will be generated.
+ */
 exports = module.exports = function(IoC, logger) {
   
   return IoC.create('app/site')
     .catch(function(err) {
-      //console.log(err)
+      // No application-specific site component is provided.  Create the default
+      // site component, which eliminates boilerplate in the application itself.
+      if (err.code == 'IMPLEMENTATION_NOT_FOUND' && err.interface == 'app/site') {
+        return IoC.create('./site');
+      }
       
-      // TODO: Check that the error is failure to create app/service
-      return IoC.create('./site');
       throw err;
     })
     .then(function(site) {
